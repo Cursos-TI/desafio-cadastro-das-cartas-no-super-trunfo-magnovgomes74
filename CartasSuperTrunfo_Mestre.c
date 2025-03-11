573cb69f1d57f6625ca9e80fcd8a7905b89719c0
Magno_V_Gomes <magnovgomes74@gmail.com>
2025-03-11 15:20:44 +0000

    atualizando o nível novato

--- CartasSuperTrunfo_Mestre.c
+++ CartasSuperTrunfo_Mestre.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo Nível Novato - Países
+// Desafio Super Trunfo Nível Básico - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
